258a9cdece89880bf581cc3ea613bd94befff984
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-07-28 21:45:04 +0300

    finish lesson 12

---
+++ lesson-12/test-function-example/funcs/isLeapYear.js
@@ -0,0 +1,23 @@
+const isLeapYear = (year)=> {
+    if(year === undefined){
+        throw new Error("Аргумент не передан");
+    }
+    
+    if(typeof year !== "number") {
+        throw new Error("Аргумент должен быть number");
+    }
+
+    if(year < 42) {
+        throw new Error("Год не может быть меньше 42");
+    }
+
+    if(!Number.isInteger(year)){
+        throw new Error("Год должен быть целым числом");
+    }
+
+    const date = new Date(year, 2, 0);
+    const days = date.getDate();
+    return (days === 29);
+}
+
+module.exports = isLeapYear;
